93263a76076747d98699e75a9be76a88b3b67deb
ardotheedu <eduardosalima3@gmail.com>
2021-04-06 17:30:06 -0300

    feat: components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import {Header} from './Components/Header'
+import {Dashboard} from './Components/Dashboard'
 import {GlobalStyle} from './styles/global'
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
 
